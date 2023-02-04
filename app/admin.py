b53e1c6861f3eedbfa80b04fa8622d36017b7df5
hadi <h.hadi.ghasemi1350@gmail.com>
2023-02-05 00:41:59 +0330

    expense and Income table created

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Expens ,Income
 
+admin.site.register(Expens)
+admin.site.register(Income)
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Expens(models.Model):
+    text=models.CharField(max_length=200)
+    created=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} : {} - {} -{}".format(self.user,self.text,self.amount,self.created)
+    
+
+class Income(models.Model):
+    text=models.CharField(max_length=200)
+    created=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} : {} - {} -{}".format(self.user,self.text,self.amount,self.created)    
+    
+

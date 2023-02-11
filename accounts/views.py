2bc3f46a878f5bfe70713edc13d418401b9ff2d0
hadi <hadi.hadi.oc@gmail.com>
2023-02-11 16:12:27 +0330

    edit login and register file + meke htmls better

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render ,redirect
-from .forms import RegisterForm ,LoginForm
+from .forms import RegisterForm , LoginForm
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate ,login
@@ -11,10 +11,8 @@ def register(request):
     if request.method == 'POST':
         form=RegisterForm(request.POST)
         if form.is_valid():
-            cd=form.cleaned_data
-            print(cd)
+            cd= form.cleaned_data
             User.objects.create_user(cd['username'],cd['email'],cd['password'])
-            messages.success(request,"user created")
             return redirect('login')
     else:
         form=RegisterForm()
@@ -29,7 +27,6 @@ def loginPage(request):
             user=authenticate(request,username=cd['username'],password=cd['password'])
             if user is not None:
                 login(request,user)
-                messages.success(request , "loged in")
                 return redirect('index')
             else:
                 print("erore")
